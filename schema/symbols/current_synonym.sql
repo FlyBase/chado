78164e69176f58e08a73776475a986a37b4cc5e6
Josh Goodman <jogoodma@iu.edu>
2019-04-24 13:58:33 -0400

    Added support for stock genotypes.
    This commit adds support for obtaining ASCII stock genotypes
    from Chado.  The non ASCII version utilizes the FlyBase
    specific format with '@' and IDs which is not very user friendly.
    
    It also switches from using an if then else branch to a case
    search for resolving which columns and tables to use for symbol
    and fullname queries.

--- schema/symbols/current_synonym.sql
+++ schema/symbols/current_synonym.sql
@@ -8,47 +8,67 @@ create or replace function flybase.current_synonym(id text, synonym_type text)
 returns varchar(255) as $$
 declare
   -- The synonym.synonym_sgml value to return.
-  synonym varchar(255);
+  synonym varchar(255) = NULL;
   -- Variables to vary which tables and fields we are using to resolve the synonyms.
-  linker_table text = 'feature_synonym';
-  object_table text = 'feature';
-  linker_field text = 'feature_id';
+  linker_table text;
+  object_table text;
+  linker_field text;
 begin
-  -- Switch tables and fields based on data class.
-  if upper(flybase.data_class(id)) = 'FBGG' then
-    linker_table = 'grp_synonym';
-    object_table = 'grp';
-    linker_field = 'grp_id';
-  elsif upper(flybase.data_class(id)) = 'FBSN' then
-    linker_table = 'strain_synonym';
-    object_table = 'strain';
-    linker_field = 'strain_id';
-  elsif upper(flybase.data_class(id)) = 'FBTC' then
-    linker_table = 'cell_line_synonym';
-    object_table = 'cell_line';
-    linker_field = 'cell_line_id';
-  elsif upper(flybase.data_class(id)) = 'FBHH' then
-    linker_table = 'humanhealth_synonym';
-    object_table = 'humanhealth';
-    linker_field = 'humanhealth_id';
-  elsif upper(flybase.data_class(id)) = 'FBLC' then
-    linker_table = 'library_synonym';
-    object_table = 'library';
-    linker_field = 'library_id';
-  end if;
 
-  -- Execute the query using the PostgreSQL format() function to substitute the various
-  -- table and field names.  Store result in the synonym variable.
-  execute format('
-    select s.synonym_sgml
-      from %1$I obj join %2$I linker on obj.%3$I = linker.%3$I
-                      join synonym s on linker.synonym_id = s.synonym_id
-                      join cvterm cvt on s.type_id=cvt.cvterm_id
-      where obj.uniquename = %4$L
-        and linker.is_current = true and linker.is_internal = false
-        and cvt.name = %5$L 
-    limit 1;', object_table, linker_table, linker_field, id, synonym_type)
-  into synonym;
+  -- Stocks use genotype instead of the synonym table so we need a complete different query.
+  -- Ignore requests for synonyms of stocks.
+  if upper(flybase.data_class(id)) = 'FBST' and upper(synonym_type) = 'SYMBOL' then
+    execute format('
+      select g.uniquename
+        from stock st join stock_genotype stg on (st.stock_id=stg.stock_id)
+                      join genotype g on (stg.genotype_id=g.genotype_id)
+        where st.uniquename = %1$L
+      limit 1;', id)
+    into synonym;
+
+  -- All other data classes use this.
+  else
+    -- Switch tables and fields based on data class.
+    case upper(flybase.data_class(id))
+      when 'FBGG' then
+        linker_table = 'grp_synonym';
+        object_table = 'grp';
+        linker_field = 'grp_id';
+      when 'FBSN' then
+        linker_table = 'strain_synonym';
+        object_table = 'strain';
+        linker_field = 'strain_id';
+      when 'FBTC' then
+        linker_table = 'cell_line_synonym';
+        object_table = 'cell_line';
+        linker_field = 'cell_line_id';
+      when 'FBHH' then
+        linker_table = 'humanhealth_synonym';
+        object_table = 'humanhealth';
+        linker_field = 'humanhealth_id';
+      when 'FBLC' then
+        linker_table = 'library_synonym';
+        object_table = 'library';
+        linker_field = 'library_id';
+      else
+        linker_table = 'feature_synonym';
+        object_table = 'feature';
+        linker_field = 'feature_id';
+    end case;
+
+    -- Execute the query using the PostgreSQL format() function to substitute the various
+    -- table and field names.  Store result in the synonym variable.
+    execute format('
+      select s.synonym_sgml
+        from %1$I obj join %2$I linker on obj.%3$I = linker.%3$I
+                        join synonym s on linker.synonym_id = s.synonym_id
+                        join cvterm cvt on s.type_id=cvt.cvterm_id
+        where obj.uniquename = %4$L
+          and linker.is_current = true and linker.is_internal = false
+          and cvt.name = %5$L 
+      limit 1;', object_table, linker_table, linker_field, id, synonym_type)
+    into synonym;
+  end if;
 
   -- Return the synonym.
   return synonym;
